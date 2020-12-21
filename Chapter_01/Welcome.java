c5aae1df311b521e28c8ff195ec10f99d66481ae
rjhefley <73319380+rjhefley@users.noreply.github.com>
2020-12-21 00:38:10 -0600

    Create Welcome.java

---
+++ Chapter_01/Welcome.java
@@ -0,0 +1,8 @@
+package Chapter_01;
+
+public class Welcome {
+    public static void main(String[] args) {
+        //Display message Welcome to Java! on the console
+        System.out.print("Welcome to Java!");
+    }
+}
