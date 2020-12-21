fd0f38f5c93712b201df98e2ae0e838db99fd5c3
rjhefley <73319380+rjhefley@users.noreply.github.com>
2020-12-21 12:55:46 -0600

    Edit Chapter_01 - Welcome.java

--- Chapter_01/Welcome.java
+++ Chapter_01/Welcome.java
@@ -3,6 +3,6 @@ package Chapter_01;
 public class Welcome {
     public static void main(String[] args) {
         //Display message Welcome to Java! on the console
-        System.out.print("Welcome to Java!");
+        System.out.println("Welcome to Java!");
     }
 }
